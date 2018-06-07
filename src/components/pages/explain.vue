459d6eb3aa12b930dcc8b1efa6b97d8837c5457b
ijinxin <874450097@qq.com>
2018-06-07 19:22:05 +0800

    next plan

--- src/components/pages/explain.vue
+++ src/components/pages/explain.vue
@@ -11,8 +11,7 @@
         <div>
             <p>下阶段：</p>
             <ul>
-                <li>学习koa2</li>
-                <li>restful api，添加注册/登录功能，首页完善</li>
+                <li>内容填充</li>
                 <li>webpack扩展</li>
             </ul>
         </div>
